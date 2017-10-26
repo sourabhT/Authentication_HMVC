e49dde40052b89b39a5587f5d84e65e621c553dd
sourabhT <sourabh.turkar@gmail.com>
2017-10-26 22:08:30 +0530

    Update topnavigation.php

--- application/views/siteadmin/topnavigation.php
+++ application/views/siteadmin/topnavigation.php
@@ -14,16 +14,13 @@
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="profile-ava">
                                 <i class="icon_profile"></i>
-<!--                                <img alt="" src="../../ina/themes/backend/img/avatar1_small.jpg">-->
+                                <img alt="" src="avatar1_small.jpg">
                             </span>
                             <span class="username"><?php echo $this->session->userdata('fullname'); ?></span>
                             <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu extended logout">
                             <div class="log-arrow-up"></div>
-                            <li class="eborder-top">
-                                <a href="<?php echo WEBSITE_URL; ?>siteadmin/users/change_password"><i class="icon_profile"></i> Change Password</a>
-                            </li>
                             <li>
                                 <a href="<?php echo WEBSITE_URL; ?>siteadmin/logout"><i class="icon_key_alt"></i> Log Out</a>
                             </li>
